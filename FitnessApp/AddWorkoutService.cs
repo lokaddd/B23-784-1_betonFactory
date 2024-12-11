7b2dac0c9ceed40215a82790101b270dbbb0e4b7
callmestfu <49409400+lokaddd@users.noreply.github.com>
2024-12-11 21:08:25 +0400

    Delete FitnessApp/AddWorkoutService.cs

--- FitnessApp/AddWorkoutService.cs
+++
@@ -1,8 +0,0 @@
-public class AddWorkoutService
-{
-    public void AddWorkout(string userId, Workout workout)
-    {
-        // Пример: добавление тренировки в базу данных
-        Console.WriteLine($"Workout for user {userId} added: {workout.Name} at {workout.Date}");
-    }
-}
